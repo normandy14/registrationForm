8813300908bf7cf59be46dcd6074c00ae06b8d3d
normandy14 <sturdyprogrammer@gmail.com>
2022-11-07 13:02:27 -0500

    Create templates and static folders for flask app. Set up form for root of app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/register")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/")
+def html_form():
+     return render_template("app.html")
 
 if __name__ == "__main__":
    app.run()
