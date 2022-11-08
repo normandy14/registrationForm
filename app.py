39b9963a7683013553f66c594298e4176fed2fa1
normandy14 <sturdyprogrammer@gmail.com>
2022-11-08 16:44:19 -0500

    Commented out password field from being required. Setup POST route. Move code in /form to root route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import mysql.connector
 
@@ -13,10 +13,17 @@ cnx = mysql.connector.connect(user='normandy14', password=pass_,
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def html_form():
-     return render_template("app.html")
-     
+    if request.method == 'POST':
+        """modify/update the information for <user_id>"""
+        data = request.form
+        print (data)
+        firstName = request.form['firstName']
+        print (firstName)
+    return render_template("app.html")
+
+''''
 @app.route("/form")
 def form():
     sql = "SELECT * FROM persons"
@@ -25,12 +32,14 @@ def form():
     myresult = cur.fetchall()
     for x in myresult:
         print(x)
-      
+    
+    
     sql = "INSERT INTO persons (firstname, lastname, email, account) VALUES ('Bob', 'Blue', 'blue@gmail.com', 'business');"
     cur = cnx.cursor()
     cur.execute(sql)
     cnx.commit()
     return ""
+'''
 
 if __name__ == "__main__":
    app.run()
